179309dc62f9a0abe3d77c61eb4d94f35e3a51d5
ajs248 <ajs248>
2001-11-03 19:46:41 +0000

    GUI == frames

---
+++ CloseableFrame.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class CloseableFrame extends Frame {
+  public CloseableFrame() {
+    addWindowListener(new WindowAdapter() {public void 
+        windowClosing(WindowEvent e ) {System.exit(0); }} );
+    setSize(300,200);
+		setLocation(300,200);
+  }
+	public void sTitle(String name) {
+    setTitle(name);
+	}
+  public static void main(String[] args) {
+    CloseableFrame c = new CloseableFrame();
+    c.show();
+  }
+}
+
+
+
