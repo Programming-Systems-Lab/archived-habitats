cc0a82e49d1952e8c5bc22460f7b8b12c4f91979
christy <christy>
2001-11-16 21:30:13 +0000

    front end for habitats  (initial version)

--- FrontEnd.java
+++ FrontEnd.java
@@ -1,5 +1,26 @@
 
-public class FrontEnd {
+/**
+ * FrontEnd.java
+ *
+ *
+ * Created: Thu Nov 15 09:26:46 2001
+ *
+ * @author Christy Lauridsen
+ * @version
+ */
 
-    public FrontEnd() {}
-}
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FrontEnd
+{
+    public static void main(String[] args)
+    {
+	FrontEndFrame frame = new FrontEndFrame();
+	frame.setVisible(true);
+    }
+} // FrontEnd
+
+
+
---
+++ FrontEndPanel.java
@@ -0,0 +1,65 @@
+/**
+ * FrontEndPanel
+ *
+ *
+ * Created: Thu Nov 15 09:23:42 2001
+ *
+ * @author Christy Lauridsen
+ * @version
+ */
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+class FrontEndPanel extends JPanel
+{
+    public FrontEndPanel()
+    {
+	setLayout(new BorderLayout());
+	add(new TextAreaPanel(), BorderLayout.CENTER);
+	add(new InputPanel(), BorderLayout.SOUTH);
+    }
+    
+    /////// Private data /////
+    private JTextArea h_textArea    = new JTextArea(8, 40);
+    private JButton h_execButton = new JButton("Execute");
+    private JTextField h_execField  = new JTextField(20);
+    
+    /////// Inner classes /////
+    class TextAreaPanel extends JPanel
+    {
+	public TextAreaPanel()
+	{
+	    setLayout( new BorderLayout() );
+	    h_textArea = new JTextArea(8, 40);
+	    h_textArea.setText(">");
+	    JScrollPane scrollPane = new JScrollPane(h_textArea);
+	    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+	    scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	    add(scrollPane, BorderLayout.CENTER);
+	}
+    }
+    
+    class InputPanel extends JPanel
+    {
+	public InputPanel()
+	{
+	    add(new JLabel("Command:"));
+	    add(h_execField);
+	    add(h_execButton);
+	    h_execButton.addActionListener(new ActionListener()
+					      {
+						  public void actionPerformed(ActionEvent ev)
+						      {
+							  String h_exec = h_execField.getText();
+							  h_textArea.append(h_exec + "\n>");
+							  h_execField.setText("");
+						      }
+					      }
+					   );
+	}
+    }
+} // FrontEndPanel
