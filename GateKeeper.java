63dff787dbf001a96615a172fc62a5f64e24df3e
ajs248 <ajs248>
2001-11-26 06:04:40 +0000

    added a method to process incomingRequest in Gatekeeper

--- GateKeeper.java
+++ GateKeeper.java
@@ -45,18 +45,22 @@ public class GateKeeper implements Notifiable {
 		String av = rav.stringValue();
 		if(av == "ServiceRequest");
 			processServiceRequest(n);
+		if(av == "Response2ServiceRequest");
+			processServiceRequest(n);
 		if(av == "currentSummit");
 			processSummitEvent(n);
 	}
   public void notify(Notification[] e) {}
 
-	public void broadcast_req(siena.Notification n) {
+	public void broadcast_req(String serName, siena.Notification n) {
 		n.putAttribute("source", masterHabitat);
+		n.putAttribute("requestingService", serName);
 		try {
 			n.publish();
 		} catch(siena.InvalidHandleException se) {
 			se.printStackTrace();
 		}
+		currServiceReqList.put(serName, n);
 	}
 
 	private void create_treaty(String src, String dest, int ID, java.lang.Object ip_param, java.lang.Object ret_param) {
@@ -87,12 +91,83 @@ public class GateKeeper implements Notifiable {
 		// assign an ID number
 		// create a new treaty and add it to the list of sumits
 		// if security cleared create a proxy of the service object
+
+		String srcHabitat, srcService;
+		AttributeValue av = n.getAttribute("source");
+		if (av != null) 
+		srcHabitat = av.stringValue();
+		av = n.getAttribute("requestingService");
+		if (av != null) 
+		srcService = av.stringValue();
+		av = n.getAttribute("totalPpparams");
+		if (av != null) 
+		int totalIP = av.intValue();
+		av = n.getAttribute("totaOPpparams");
+		if (av != null) 
+		int totalOP = av.intValue();
+
+		Notification n = new Notification();
+		if(allowed(srcHabitat,srcService) {
+			// send a +ve response
+			int ID = getNextID();
+			n.putAttribute("habitatName", srcHabitat);
+			n.putAttribute("EventType", "Response2ServiceRequest");
+			n.putAttribute("Access", "Allowed");
+			n.putAttribute("Treaty", "AcceptedAll");	
+			/* AcceptAll = all the ip_params and op_params sent in the 
+			*  request were allowed.
+			*  New Treaty = the new treaty description follows thsi event 
+			*  that gives a new description of the ip_params and the op_params
+			*  currently only AcceptAll implemented
+			*/
+			n.putAttribute("requestingService" , srcService);
+			n.putAttribute("IDnumber", ID); 
+			
+			Vector ip = new Vector();
+			Vector op = new Vector();
+			for (int ind =1; ind <= totalIPparams; ind++) {
+				String att = "param";
+				att += ind;
+				av = n.getAttribute(att);
+				if (av != null) {
+					ip.add(av.stringValue())
+				}
+			}
+			for (ind =1; ind <= totalOPparams; ind++) {
+				String att = "retParam";
+				att += ind;
+				av = n.getAttribute(att);
+				if (av != null) {
+					op.add(av.stringValue())
+				}
+			}
+
+			create_treaty(srcHabitat, roleName, ID, ip, op);
+			// create a new treaty and a new proxy for the requested service
+		}
+		else {
+			// send a -ve response
+			n.putAttribute("habitatName", srcHabitat);
+			n.putAttribute("EventType", "Response2ServiceRequest");
+			n.putAttribute("Access", "Denied");
+		}
+		try {
+			s.publish(n);
+		} catch (siena.SienaException se) {
+			se.printStackTrace();
+		}
 	}
+	public void processResponse2ServiceRequest(Notification n) {
+	// response to the service request
+	// create a treaty with the params 
+	// save the ID number recieved by the remote GK
+	// notify the service to start using the service
+	}
+
 	public void processSummitEvent(Notification n) {
 		// get the ID 
 		// gk contacts the summitlist to check if there if it should process it.
 		// check if the i/p param are valid
 		// contact the service and call forward mesg with results.
 	}
-
 }
